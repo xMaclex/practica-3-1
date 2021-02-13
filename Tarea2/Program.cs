2271e1b7d6ade4c691a185ed359b28bc73878f38
xMaclex <62273275+xMaclex@users.noreply.github.com>
2021-02-12 22:41:46 -0400

    Add files via upload

---
+++ Tarea2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n <=10);
+        }
+    }
+}
