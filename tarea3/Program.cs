56382c6182555653932812c4fe9bf78d9766f0a8
xMaclex <62273275+xMaclex@users.noreply.github.com>
2021-02-12 23:24:35 -0400

    Add files via upload

---
+++ tarea3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tarea3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 10;
+            int A = 10;
+            int resultado = m * A;
+            Console.WriteLine(resultado);
+            for (int n = 2; n <=20; n++)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+
+        }
+    }
+}
