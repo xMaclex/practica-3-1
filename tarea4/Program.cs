3769c72145dc31698aec8c6a5a47722fbec36201
xMaclex <62273275+xMaclex@users.noreply.github.com>
2021-02-12 23:52:43 -0400

    Add files via upload

---
+++ tarea4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tarea4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                char letra;
+ 
+            for (letra='A'; letra<='Z'; letra++)
+            Console.Write("{0} ", letra);
+        }
+    }
+}
